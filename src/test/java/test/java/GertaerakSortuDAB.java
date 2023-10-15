0ab2d609e947fe3048ae937ee346dfd9f2e77319
mikelmujika16 <mmugica053@ikasle.ehu.eus>
2023-10-15 14:57:11 +0200

    ja

---
+++ src/test/java/test/java/GertaerakSortuDAB.java
@@ -0,0 +1,14 @@
+package test.java;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GertaerakSortuDAB {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
